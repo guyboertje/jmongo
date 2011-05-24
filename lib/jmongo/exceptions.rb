a41c02a03eb83598032d0fbe02b450b1ab016c51
Lee Henson <lee.m.henson@gmail.com>
2011-05-24 14:22:47 +0100

    Added OperationFailure exception type

--- lib/jmongo/exceptions.rb
+++ lib/jmongo/exceptions.rb
@@ -1,4 +1,10 @@
 module Mongo
+#  class MongoRubyError < StandardError; end
+#  class MongoDBError < RuntimeError; end
+#  class ConnectionError < MongoRubyError; end
+#  class OperationFailure < MongoDBError; end
+
   class MongoDBError  < Java::ComMongodb::MongoException; end
   class ConnectionError < Java::ComMongodb::MongoException::Network; end
+  class OperationFailure < Java::ComMongodb::MongoException::DuplicateKey; end
 end
